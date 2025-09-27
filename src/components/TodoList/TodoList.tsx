bde4d91c8919b787f56adf5f08f5cb26d05acf41
Polianskyi Dmytro <dimdemon1@gmail.com>
2025-09-27 10:36:22 +0300

    change ternary to cn liblrary

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
+import cn from 'classnames';
 
 interface Props {
   todos: Todo[];
@@ -39,9 +40,10 @@ export const TodoList: React.FC<Props> = ({
           </td>
           <td className="is-vcentered is-expanded">
             <p
-              className={
-                todo.completed ? 'has-text-success' : 'has-text-danger'
-              }
+              className={cn({
+                'has-text-success': todo.completed,
+                'has-text-danger': !todo.completed,
+              })}
             >
               {todo.title}
             </p>
@@ -57,9 +59,10 @@ export const TodoList: React.FC<Props> = ({
             >
               <span className="icon">
                 <i
-                  className={
-                    todo === selectedTodo ? 'far fa-eye-slash' : 'far fa-eye'
-                  }
+                  className={cn('far', {
+                    'fa-eye-slash': todo === selectedTodo,
+                    'fa-eye': todo !== selectedTodo,
+                  })}
                 />
               </span>
             </button>
